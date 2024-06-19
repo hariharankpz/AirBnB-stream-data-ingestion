8b71145ecc559f12126de3e824dd3e359c2f4817
Hariharan <hariharan97b@gmail.com>
2024-06-19 12:03:37 +0530

    added some comments

--- enrichment-filter/lamdba_function.py
+++ enrichment-filter/lamdba_function.py
@@ -1,5 +1,5 @@
 import json
-
+#SQS will give one message at a time to lamda this function, because we set batch size = 1 in sqs properties
 def lambda_handler(event, context):
     try:
         print('Event: ', event)
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         print(event)
         message =  json.loads(event[0]['body'])
         print(message)
-        if (message['startDate'] == message['endDate']):
+        if (message['startDate'] == message['endDate']):#One msg at a time is feeded here in the lamda fuction from SQS
             message = {}
         return {
             'message': message
